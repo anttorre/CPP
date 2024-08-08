88acac1bdaaae8e7ac3d6ecab29b94afa46e1d92
Antonio Torres Mora <anttorre@c2r9s4.42malaga.com>
2024-08-08 17:17:54 +0200

    check dates

--- CPP-09/ex00/BitcoinExchange.cpp
+++ CPP-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: anttorre <anttorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:19:50 by anttorre          #+#    #+#             */
-/*   Updated: 2024/08/07 17:10:04 by anttorre         ###   ########.fr       */
+/*   Updated: 2024/08/08 17:16:40 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,10 @@ bool BitcoinExchange::check_files(std::string file, std::string db)
 {
 	std::ifstream f1(file.c_str());
 	std::ifstream f2(db.c_str());
-	return f1.is_open() && f2.is_open();
+	bool open = f1.is_open() && f2.is_open();
+	f1.close();
+	f2.close();
+	return open;
 }
 
 void	BitcoinExchange::savedb(std::string db)
@@ -60,12 +63,49 @@ void	BitcoinExchange::savedb(std::string db)
 		std::string value(line.substr(indexComa + 1, line.length() - indexComa + 1));
 		this->_data.insert(std::pair<time_t, double>(makeDate(key), atof(value.c_str())));
 	}
+	f1.close();
 }
 
 bool BitcoinExchange::check_file(std::string f)
 {
 	std::ifstream f1(f.c_str());
+	std::string line;
+	std::string date;
+	std::string value;
+	int badInput = 0;
+	while (std::getline(f1, line))
+	{
+		date = trim(line.substr(0, line.find("|")));
+		value = trim(line.substr(line.find("|") + 1, line.length() - line.find("|") + 1));
+		try
+		{
+			check_date(makeDate(date), &badInput);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		
+	}
+	f1.close();
 	return true;
+}
+
+void	BitcoinExchange::check_date(time_t t, int *f)
+{
+	tm* timeinfo = localtime(&t);
+	timeinfo->tm_year += 1900;
+	timeinfo->tm_mon += 1;
+	if (timeinfo->tm_year < 2009)
+	{
+		*f = 1;
+		throw BadInput();
+	}
+	else if (timeinfo->tm_mon < 1 || timeinfo->tm_mon > 12)
+	{
+		*f = 1;
+		throw BadInput();
+	}
 }
 
 time_t BitcoinExchange::makeDate(const std::string& fechaStr)
@@ -78,4 +118,19 @@ time_t BitcoinExchange::makeDate(const std::string& fechaStr)
     fecha.tm_year -= 1900;
     fecha.tm_mon -= 1;
     return mktime(&fecha);
+}
+
+const char * BitcoinExchange::NegativeValue::what() const throw()
+{
+	return "Error: not a positive number.";
+}
+
+const char * BitcoinExchange::BadInput::what() const throw()
+{
+	return "Error: bad input";
+}
+
+const char * BitcoinExchange::LargeNumber::what() const throw()
+{
+	return "Error: too large a number.";
 }
--- CPP-09/ex00/BitcoinExchange.hpp
+++ CPP-09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: anttorre <anttorre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 13:07:32 by anttorre          #+#    #+#             */
-/*   Updated: 2024/08/07 17:09:54 by anttorre         ###   ########.fr       */
+/*   Updated: 2024/08/08 16:46:39 by anttorre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ class BitcoinExchange
 		void	savedb(std::string db);
 		bool	check_file(std::string f);
 		time_t makeDate(const std::string& fechaStr);
+		void	check_date(time_t t, int *f);
 	
     public:
         BitcoinExchange();
@@ -34,6 +35,30 @@ class BitcoinExchange
         ~BitcoinExchange();
         BitcoinExchange& operator=(BitcoinExchange &other);
 		void	init(std::string file, std::string db);
+		class NegativeValue : public std::exception
+		{
+			const char * what() const throw();
+		};
+		class BadInput : public std::exception
+		{
+			const char * what() const throw();
+		};
+		class LargeNumber : public std::exception
+		{
+			const char * what() const throw();
+		};
 };
+
+static std::string trim(const std::string& str) {
+    std::string::const_iterator start = str.begin();
+    while (start != str.end() && std::isspace(*start)) {
+        ++start;
+    }
+    std::string::const_iterator end = str.end();
+    do {
+        --end;
+    } while (end != start && std::isspace(*end));
+    return std::string(start, end + 1);
+}
 
 #endif
